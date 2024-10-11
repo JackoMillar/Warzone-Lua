c0ee720ce5d74e2e756c198e4a610428775f8119
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-10-11 13:12:46 +0100

    Update Client_GameRefresh.lua

--- ConstructionWorker/Client_GameRefresh.lua
+++ ConstructionWorker/Client_GameRefresh.lua
@@ -3,7 +3,7 @@ Alerted = false;
 function Client_GameRefresh(game)
     
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
-		UI.Alert("You must update your app to the latest version to use the Tanks mod");
+		UI.Alert("You must update your app to the latest version to use the ConstructionWorker mod");
         Alerted = true;
 	end
 
