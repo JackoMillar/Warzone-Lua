0f0e91696d1974dd23278554a79b0a66188e9a39
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-10-11 13:14:36 +0100

    Update Client_GameRefresh.lua

--- Warzone2069/Client_GameRefresh.lua
+++ Warzone2069/Client_GameRefresh.lua
@@ -18,7 +18,7 @@ end
 end
 
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
-		UI.Alert("You must update your app to the latest version to use the Special Units Structures modpack");
+		UI.Alert("You must update your app to the latest version to use the Warzone2069 mod");
         Alerted = true;
 	end
 	if(game.Us == nil)then
