f290dbc147516d6eface22b19022edfcdc3c2033
Dan <daniel.church@btinternet.com>
2025-01-02 21:17:41 +0000

    apply other formatting changes to all files in ArmyRecruiter

--- ArmyRecruiter/Client_SaveConfigureUI.lua
+++ ArmyRecruiter/Client_SaveConfigureUI.lua
@@ -1,11 +1,16 @@
 function Client_SaveConfigureUI(alert)
 	local cost = costInputField.GetValue();
-	if cost < 1 then alert("the cost to buy a Recruiter must be positive"); end
-	Mod.Settings.CostToBuyRecruiter = cost;
-
 	local maxRecruiters = maxRecruitersField.GetValue();
-	if maxRecruiters < 1 or maxRecruiters > 5 then alert("Max number of Recruiters per player must be between 1 and 5"); end
-	Mod.Settings.MaxRecruiters = maxRecruiters;
 
+	if cost < 1 then
+		alert('the cost to buy a Recruiter must be positive');
+	end
+
+	if maxRecruiters < 1 or maxRecruiters > 5 then
+		alert('Max number of Recruiters per player must be between 1 and 5');
+	end
+
+	Mod.Settings.CostToBuyRecruiter = cost;
+	Mod.Settings.MaxRecruiters = maxRecruiters;
 	Mod.Settings.NumArmies = armiesField.GetValue();
 end
