1d9cfb42f57f993c0704e9a4e67af7150aa2a894
Dan <daniel.church@btinternet.com>
2025-01-02 18:14:01 +0000

    apply .editorconfig to all files in Warzone2069

--- Warzone2069/Client_GameRefresh.lua
+++ Warzone2069/Client_GameRefresh.lua
@@ -2,32 +2,31 @@ require('LoreBook');
 Alerted = false;
 
 function Client_GameRefresh(game)
- local data = Mod.PublicGameData; 
-  Game = game;
- local Viewed = false;	
-  for p, player in pairs(game.Game.PlayingPlayers) do
- if data.Gullible[p] == true then
-	Viewed = true;
-   if data.Viewed[game.Us.ID] == false then
-	local name = player.DisplayName(nil, false)	
-	UI.Alert(gullible(name));
-	   Game.SendGameCustomMessage('Player Viewed', {type = "UpdatingViewed", p = data.Viewed[Game.Us.ID]}, function(placeholder)
-                                                                                                                                 end);
- end
-end
-end
+	local data = Mod.PublicGameData;
+	Game = game;
+	local Viewed = false;
+	for p, player in pairs(game.Game.PlayingPlayers) do
+		if data.Gullible[p] == true then
+			Viewed = true;
+			if data.Viewed[game.Us.ID] == false then
+				local name = player.DisplayName(nil, false)
+				UI.Alert(gullible(name));
+				Game.SendGameCustomMessage('Player Viewed', {type = "UpdatingViewed", p = data.Viewed[Game.Us.ID]}, function(placeholder) end);
+			end
+		end
+	end
 
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
 		UI.Alert("You must update your app to the latest version to use the Warzone2069 mod");
-        Alerted = true;
+		Alerted = true;
 	end
 	if(game.Us == nil)then
 		return;
 	end
-if Viewed == false then
-  	if game.Game.TurnNumber >= 2 then
-         	local advert = advert();
-         	UI.Alert(advert);		
-   	end
-end
+	if Viewed == false then
+		if game.Game.TurnNumber >= 2 then
+			local advert = advert();
+			UI.Alert(advert);
+		end
+	end
 end
--- Warzone2069/Server_AdvanceTurn.lua
+++ Warzone2069/Server_AdvanceTurn.lua
@@ -1,47 +1,47 @@
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
-   if order.proxyType == "GameOrderAttackTransfer" then
-    if orderResult.IsSuccessful then
-    local r = math.random(1,25);
-    local list = {};
-	if (r == 25) then
-		local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-		local attackerTerr = game.ServerGame.LatestTurnStanding.Territories[order.From];		
-          	local terrModTo = WL.TerritoryModification.Create(order.To); 		
-		terrModTo.SetOwnerOpt = attackedTerr.OwnerPlayerID;
-		terrModTo.SetArmiesTo = (attackedTerr.NumArmies.NumArmies);
-              
-		local terrModfrom = WL.TerritoryModification.Create(order.From);
-		terrModfrom.SetArmiesTo = (attackerTerr.NumArmies.NumArmies);
-				
-		table.insert(list, terrModfrom);
-		table.insert(list, terrModTo);
-		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Rubberbanding",{}, list), true);
-     	end
-   end
- end
+	if order.proxyType == "GameOrderAttackTransfer" then
+		if orderResult.IsSuccessful then
+			local r = math.random(1,25);
+			local list = {};
+			if (r == 25) then
+				local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To];
+				local attackerTerr = game.ServerGame.LatestTurnStanding.Territories[order.From];
+				local terrModTo = WL.TerritoryModification.Create(order.To);
+				terrModTo.SetOwnerOpt = attackedTerr.OwnerPlayerID;
+				terrModTo.SetArmiesTo = (attackedTerr.NumArmies.NumArmies);
+
+				local terrModfrom = WL.TerritoryModification.Create(order.From);
+				terrModfrom.SetArmiesTo = (attackerTerr.NumArmies.NumArmies);
+
+				table.insert(list, terrModfrom);
+				table.insert(list, terrModTo);
+				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Rubberbanding",{}, list), true);
+			end
+		end
+	end
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-local data = Mod.PublicGameData;
-local count = 0;
-local t = {};
-for p, player in pairs(game.Game.PlayingPlayers) do
-	local IncomeAmount = 1;
-	addNewOrder(WL.GameOrderEvent.Create(p, "Paying Taxes", {}, {}, {}, {WL.IncomeMod.Create(p, -IncomeAmount, "Paying taxes")}));
-	if data.Viewed[p] == true or player.IsAI == true then
-		table.insert(t, data.Viewed[p]);
+	local data = Mod.PublicGameData;
+	local count = 0;
+	local t = {};
+	for p, player in pairs(game.Game.PlayingPlayers) do
+		local IncomeAmount = 1;
+		addNewOrder(WL.GameOrderEvent.Create(p, "Paying Taxes", {}, {}, {}, {WL.IncomeMod.Create(p, -IncomeAmount, "Paying taxes")}));
+		if data.Viewed[p] == true or player.IsAI == true then
+			table.insert(t, data.Viewed[p]);
+		end
+		count = count + 1;
 	end
-	count = count + 1;
-end
-print(count);
-print(#t);
-for p, _ in pairs(game.Game.PlayingPlayers) do
-	if count == #t then
-	   print(22);
-	   data.Gullible[p] = false;
-	   data.Viewed[p] = false;
-        end
-end
+	print(count);
+	print(#t);
+	for p, _ in pairs(game.Game.PlayingPlayers) do
+		if count == #t then
+			print(22);
+			data.Gullible[p] = false;
+			data.Viewed[p] = false;
+		end
+	end
 
-Mod.PublicGameData = data;
+	Mod.PublicGameData = data;
 end
