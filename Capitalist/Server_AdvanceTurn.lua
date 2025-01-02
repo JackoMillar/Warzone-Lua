7c273374af3a1230265b3dbe49baefa08e6e79c6
Dan <daniel.church@btinternet.com>
2025-01-02 21:50:50 +0000

    apply other formatting changes to all files in Capitalist

--- Capitalist/Server_AdvanceTurn.lua
+++ Capitalist/Server_AdvanceTurn.lua
@@ -1,85 +1,116 @@
 require('Utilities');
 
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
-	if (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'BuyCapitalist_')) then  --look for the order that we inserted in Client_PresentCommercePurchaseUI
-		--in Client_PresentMenuUI, we stuck the territory ID after BuyCapitalist_.  Break it out and parse it to a number.
-		local targetTerritoryID = tonumber(string.sub(order.Payload, 15));
-		print(string.sub(order.Payload, 15));
-		print(targetTerritoryID);
-		local targetTerritoryStanding = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID];
-		if (targetTerritoryStanding.OwnerPlayerID ~= order.PlayerID) then
-			return; --can only buy a priest onto a territory you control
-		end
+	buyCapitalist(game, order, orderResult, skipThisOrder, addNewOrder);
+	checkForDeadCapitalist(game, order, orderResult, skipThisOrder, addNewOrder);
+end
 
-		if (order.CostOpt == nil) then
-			return; --shouldn't ever happen, unless another mod interferes
-		end
-
-		local costFromOrder = order.CostOpt[WL.ResourceType.Gold]; --this is the cost from the order.  We can't trust this is accurate, as someone could hack their client and put whatever cost they want in there.  Therefore, we must calculate it ourselves, and only do the purchase if they match
-		local realCost = Mod.Settings.CostToBuyCapitalist;
-
-		if (realCost > costFromOrder) then
-			return; --don't do the purchase if their cost didn't line up.  This would only really happen if they hacked their client or another mod interfered
-		end
-
-		local numCapitalistsAlreadyHave = 0;
-		for _,ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			if (ts.OwnerPlayerID == order.PlayerID) then
-				numCapitalistsAlreadyHave = numCapitalistsAlreadyHave + NumCapitalistsIn(ts.NumArmies);
-			end
-		end
-
-		if (numCapitalistsAlreadyHave >= Mod.Settings.MaxCapitalists) then
-			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipping Capitalist purchase since max is ' .. Mod.Settings.MaxCapitalists .. ' and you have ' .. numCapitalistsAlreadyHave));
-			return; --this player already has the maximum number of Capitalists possible, so skip adding a new one.
-		end
-
-		local CapitalistPower = Mod.Settings.CapitalistPower;
-
-		local builder = WL.CustomSpecialUnitBuilder.Create(order.PlayerID);
-		builder.Name = 'Capitalist';
-		builder.IncludeABeforeName = true;
-		builder.ImageFilename = 'piggy-bank.png';
-		builder.AttackPower = 1;
-		builder.DefensePower = 1;
-		builder.CombatOrder = 3415; --defends commanders
-		builder.DamageToKill = 1;
-		builder.DamageAbsorbedWhenAttacked = 1;
-		builder.CanBeGiftedWithGiftCard = true;
-		builder.CanBeTransferredToTeammate = true;
-		builder.CanBeAirliftedToSelf = true;
-		builder.CanBeAirliftedToTeammate = true;
-		builder.IsVisibleToAllPlayers = false;
-
-		local terrMod = WL.TerritoryModification.Create(targetTerritoryID);
-		terrMod.AddSpecialUnits = {builder.Build()};
-
-		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Purchased a Capitalist', {}, {terrMod}));
+function buyCapitalist(game, order, orderResult, skipThisOrder, addNewOrder)
+	if not (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'BuyCapitalist_')) then
+		return;
 	end
-	if order.proxyType == "GameOrderAttackTransfer" then
-		if orderResult.IsAttack and hasNoCapitalist(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies) then
-			if deadCapitalist(orderResult.DefendingArmiesKilled) then
-				local p = order.PlayerID; -- the attacker
 
-				local currentIncome = game.Game.PlayingPlayers[p].Income(0, game.ServerGame.LatestTurnStanding, false, false);
+	-- look for the order that we inserted in Client_PresentCommercePurchaseUI
+	-- in Client_PresentMenuUI, we stuck the territory ID after BuyCapitalist_.  Break it out and parse it to a number.
 
-				local IncomeAmount = currentIncome.Total;
-				print(IncomeAmount);
-				IncomeAmount = IncomeAmount * (Mod.Settings.Percentage / 100);
-				addNewOrder(WL.GameOrderEvent.Create(p, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(p, -IncomeAmount, "You have killed a Capitalist and have been sanctioned")}));
+	local targetTerritoryID = tonumber(string.sub(order.Payload, 15));
 
-			end
+	print(string.sub(order.Payload, 15));
+	print(targetTerritoryID);
+
+	local targetTerritoryStanding = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID];
+
+	if targetTerritoryStanding.OwnerPlayerID ~= order.PlayerID then
+		-- can only buy a priest onto a territory you control
+		return;
+	end
+
+	if not order.CostOpt then
+		-- shouldn't ever happen, unless another mod interferes
+		return;
+	end
+
+	local costFromOrder = order.CostOpt[WL.ResourceType.Gold]; -- this is the cost from the order.  We can't trust this is accurate, as someone could hack their client and put whatever cost they want in there.  Therefore, we must calculate it ourselves, and only do the purchase if they match
+	local realCost = Mod.Settings.CostToBuyCapitalist;
+
+	if realCost > costFromOrder then
+		-- don't do the purchase if their cost didn't line up.  This would only really happen if they hacked their client or another mod interfered
+		return;
+	end
+
+	local numCapitalistsAlreadyHave = 0;
+
+	for _, ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+		if (ts.OwnerPlayerID == order.PlayerID) then
+			numCapitalistsAlreadyHave = numCapitalistsAlreadyHave + NumCapitalistsIn(ts.NumArmies);
 		end
 	end
+
+	if numCapitalistsAlreadyHave >= Mod.Settings.MaxCapitalists then
+		-- this player already has the maximum number of Capitalists possible, so skip adding a new one.
+
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipping Capitalist purchase since max is ' .. Mod.Settings.MaxCapitalists .. ' and you have ' .. numCapitalistsAlreadyHave));
+
+		return;
+	end
+
+	local builder = WL.CustomSpecialUnitBuilder.Create(order.PlayerID);
+
+	builder.Name = 'Capitalist';
+	builder.IncludeABeforeName = true;
+	builder.ImageFilename = 'piggy-bank.png';
+	builder.AttackPower = 1;
+	builder.DefensePower = 1;
+	builder.CombatOrder = 3415; --defends commanders
+	builder.DamageToKill = 1;
+	builder.DamageAbsorbedWhenAttacked = 1;
+	builder.CanBeGiftedWithGiftCard = true;
+	builder.CanBeTransferredToTeammate = true;
+	builder.CanBeAirliftedToSelf = true;
+	builder.CanBeAirliftedToTeammate = true;
+	builder.IsVisibleToAllPlayers = false;
+
+	local terrMod = WL.TerritoryModification.Create(targetTerritoryID);
+	terrMod.AddSpecialUnits = {builder.Build()};
+
+	addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Purchased a Capitalist', {}, {terrMod}));
+end
+
+function checkForDeadCapitalist(game, order, orderResult, skipThisOrder, addNewOrder)
+	if not (order.proxyType == 'GameOrderAttackTransfer') then
+		return;
+	end
+
+	if not (orderResult.IsAttack and hasNoCapitalist(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies)) then
+		return;
+	end
+
+	if not deadCapitalist(orderResult.DefendingArmiesKilled) then
+		return;
+	end
+
+	local p = order.PlayerID; -- the attacker
+	local currentIncome = game.Game.PlayingPlayers[p].Income(0, game.ServerGame.LatestTurnStanding, false, false);
+	local IncomeAmount = currentIncome.Total;
+
+	print(IncomeAmount);
+
+	IncomeAmount = IncomeAmount * (Mod.Settings.Percentage / 100);
+
+	local incomeMod = WL.IncomeMod.Create(p, -IncomeAmount, 'You have killed a Capitalist and have been sanctioned');
+
+	addNewOrder(WL.GameOrderEvent.Create(p, 'Updated income', {}, {}, {}, {incomeMod}));
 end
 
 function NumCapitalistsIn(armies)
 	local ret = 0;
-	for _,su in pairs(armies.SpecialUnits) do
-		if (su.proxyType == 'CustomSpecialUnit' and su.Name == 'Capitalist') then
+
+	for _, su in pairs(armies.SpecialUnits) do
+		if su.proxyType == 'CustomSpecialUnit' and su.Name == 'Capitalist' then
 			ret = ret + 1;
 		end
 	end
+
 	return ret;
 end
 
@@ -89,6 +120,7 @@ function hasNoCapitalist(armies)
 			return true;
 		end
 	end
+
 	return false;
 end
 
@@ -98,6 +130,7 @@ function deadCapitalist(army)
 			return true;
 		end
 	end
+
 	return false;
 end
 
