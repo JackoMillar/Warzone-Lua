f7a6d79828bc95d2b03a3414375ef24a10ccf943
Dan <daniel.church@btinternet.com>
2025-01-02 16:23:19 +0000

    simplify logic to achive same result and multiple formatting fixes

--- CivLight/Server_StartGame.lua
+++ CivLight/Server_StartGame.lua
@@ -4,18 +4,17 @@ require('distributeRandomStructures');
 require('distributeRandomStructures2');
 
 function Server_StartGame(game, standing)
-
 	local count = 0;
 	for cardID, _ in pairs(game.Settings.Cards) do
-        count = count + 1;
-    end
-	
+		count = count + 1;
+	end
 
-if count > 0 then
-distributeRandomStructures2(standing, WL.StructureType.ResourceCache, Mod.Settings.NumOfRCaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
-end
-distributeRandomStructures2(standing, WL.StructureType.ArmyCache, Mod.Settings.NumOfACaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
-distributeRandomStructures(standing, WL.StructureType.MercenaryCamp, 100, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false, allowConnectedTerrs = false, mapDetails = game.Map});
+	if count > 0 then
+		distributeRandomStructures2(standing, WL.StructureType.ResourceCache, Mod.Settings.NumOfRCaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
+	end
+
+	distributeRandomStructures2(standing, WL.StructureType.ArmyCache, Mod.Settings.NumOfACaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
+	distributeRandomStructures(standing, WL.StructureType.MercenaryCamp, 100, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false, allowConnectedTerrs = false, mapDetails = game.Map});
 end
 
 function getRandomTerritory(territoryArray)
