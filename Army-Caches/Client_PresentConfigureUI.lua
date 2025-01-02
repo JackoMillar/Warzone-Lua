053d78178424ed6ec739e8738b954ff11eeb3691
Dan <daniel.church@btinternet.com>
2025-01-02 15:38:09 +0000

    apply .editorconfig to all files in ArmyCaches

--- Army-Caches/Client_PresentConfigureUI.lua
+++ Army-Caches/Client_PresentConfigureUI.lua
@@ -25,11 +25,11 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 	local horz1 = UI.CreateHorizontalLayoutGroup(vert);  --not used
 	local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for reference
 
-		UI.CreateLabel(vert).SetText('Amount of Army Caches that will spawn at the start of the game');
-		numberInputField = UI.CreateNumberInputField(vert)
-			.SetSliderMinValue(1)
-			.SetSliderMaxValue(20)
-			.SetValue(initialACaches);
+	UI.CreateLabel(vert).SetText('Amount of Army Caches that will spawn at the start of the game');
+	numberInputField = UI.CreateNumberInputField(vert)
+		.SetSliderMinValue(1)
+		.SetSliderMaxValue(20)
+		.SetValue(initialACaches);
 
 	UI.CreateLabel(vert).SetText('Amount of armies that you will get for claiming a cache');
 		numberInputField2 = UI.CreateNumberInputField(vert)
