dd7ae71b43dbe40c9fe61609c112b9a129ba72f5
Dan <daniel.church@btinternet.com>
2025-01-03 10:46:09 +0000

    apply other formatting changes to all files in Diplomat

--- Diplomat/Utilities.lua
+++ Diplomat/Utilities.lua
@@ -1,60 +1,66 @@
---This file contains utility functions that could be used in any lua project.
+-- This file contains utility functions that could be used in any lua project.
 
 function split(str, pat)
-	local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-	local fpat = "(.-)" .. pat
-	local last_end = 1
-	local s, e, cap = str:find(fpat, 1)
+	local t = {}; -- NOTE: use {n = 0} in Lua-5.0
+	local fpat = '(.-)' .. pat;
+	local last_end = 1;
+	local s, e, cap = str:find(fpat, 1);
+
 	while s do
-	if s ~= 1 or cap ~= "" then
-		table.insert(t,cap)
+		if s ~= 1 or cap ~= '' then
+			table.insert(t, cap);
+		end
+
+		last_end = e + 1;
+		s, e, cap = str:find(fpat, last_end);
 	end
-	last_end = e+1
-	s, e, cap = str:find(fpat, last_end)
-	end
+
 	if last_end <= #str then
-	cap = str:sub(last_end)
-	table.insert(t, cap)
+		cap = str:sub(last_end);
+		table.insert(t, cap);
 	end
-	return t
+
+	return t;
 end
 
-
 function map(array, func)
-	local new_array = {}
+	local new_array = {};
 	local i = 1;
-	for _,v in pairs(array) do
+
+	for _, v in pairs(array) do
 		new_array[i] = func(v);
 		i = i + 1;
 	end
-	return new_array
+
+	return new_array;
 end
 
-
 function filter(array, func)
-	local new_array = {}
+	local new_array = {};
 	local i = 1;
-	for _,v in pairs(array) do
-		if (func(v)) then
+
+	for _, v in pairs(array) do
+		if func(v) then
 			new_array[i] = v;
 			i = i + 1;
 		end
 	end
-	return new_array
+
+	return new_array;
 end
 
 function first(array, func)
-	for _,v in pairs(array) do
-		if (func(v)) then
+	for _, v in pairs(array) do
+		if func(v) then
 			return v;
 		end
 	end
-	return nil;
 end
 
 function randomFromArray(array)
 	local len = #array;
 	local i = math.random(len);
+
 	return array[i];
 end
 
