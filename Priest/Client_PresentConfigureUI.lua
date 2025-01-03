9daa3302ac0434f21b6c6e60146bce0a48941764
Dan <daniel.church@btinternet.com>
2025-01-03 12:41:58 +0000

    apply other formatting changes to all files in Priest
    remove code duplication from Server_AdvanceTurn

--- Priest/Client_PresentConfigureUI.lua
+++ Priest/Client_PresentConfigureUI.lua
@@ -1,69 +1,59 @@
-
 function Client_PresentConfigureUI(rootParent)
-
-	if (not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
-		UI.Alert("You must update your app to the latest version to use this mod");
+	if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
+		UI.Alert('You must update your app to the latest version to use this mod');
 		return;
 	end
 
-
-	local power = Mod.Settings.PriestPower;
-	if power == nil then power = 1; end
-
-	local cost = Mod.Settings.CostToBuyPriest;
-	if cost == nil then cost = 25; end
-
-	local maxPriests = Mod.Settings.MaxPriests;
-	if maxPriests == nil then maxPriests = 3; end;
-
-	local percentage = Mod.Settings.Percentage;
-	if percentage == nil then percentage = 50; end
-
-	local defense = Mod.Settings.Defensive;
-	if defense == nil then defense = true; end
-
-	local offence= Mod.Settings.Offensive;
-	if offence == nil then offence = true; end
+	local power = Mod.Settings.PriestPower or 1;
+	local cost = Mod.Settings.CostToBuyPriest or 25;
+	local maxPriests = Mod.Settings.MaxPriests or 3;
+	local percentage = Mod.Settings.Percentage or 50;
+	local defense = not not Mod.Settings.Defensive;
+	local offence= (Mod.Settings.Offensive == nil and true) or Mod.Settings.Offensive;
 
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
 	local row1 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row1).SetText('How much gold it costs to buy a priest');
+
+	UI.CreateLabel(row1).SetText('How much gold it costs to buy a Priest');
 	costInputField = UI.CreateNumberInputField(row1)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(40)
 		.SetValue(cost);
 
-
 	local row2 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row2).SetText('How powerful the priest is (in armies). (recommended to be 1)');
+
+	UI.CreateLabel(row2).SetText('How powerful the Priest is (in armies). (recommended to be 1)');
 	powerInputField = UI.CreateNumberInputField(row2)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(30)
 		.SetValue(power);
 
 	local row3 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row3).SetText('How many priests each player can have at a time');
+
+	UI.CreateLabel(row3).SetText('How many Priests each player can have at a time:');
 	maxPriestsField = UI.CreateNumberInputField(row3)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(5)
 		.SetValue(maxPriests);
 
 	local row4 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row4).SetText('percentage of armies converted');
+
+	UI.CreateLabel(row4).SetText('Percentage of armies converted:');
 	percentageField = UI.CreateNumberInputField(row4)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(100)
 		.SetValue(percentage);
 
 	local row5 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row5).SetText('priest conversion activates when he is attacked:');
+
+	UI.CreateLabel(row5).SetText('Priest conversion activates when they are attacked:');
 	defenseField = UI.CreateCheckBox(row5)
 		.SetIsChecked(defense);
 
 	local row6 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row6).SetText('priest conversion activates when he attacks');
+
+	UI.CreateLabel(row6).SetText('Priest conversion activates when they attack:');
 	offenceField = UI.CreateCheckBox(row6)
 		.SetIsChecked(offence);
-
 end
--- Priest/Server_AdvanceTurn.lua
+++ Priest/Server_AdvanceTurn.lua
@@ -1,110 +1,140 @@
 require('Utilities');
 
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
-	if (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'BuyPriest_')) then
-		--look for the order that we inserted in Client_PresentCommercePurchaseUI
-		--in Client_PresentMenuUI, we stuck the territory ID after BuyPriest_.  Break it out and parse it to a number.
-		local targetTerritoryID = tonumber(string.sub(order.Payload, 11));
-		print(string.sub(order.Payload, 11));
-		local targetTerritoryStanding = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID];
-		if (targetTerritoryStanding.OwnerPlayerID ~= order.PlayerID) then
-			return; --can only buy a priest onto a territory you control
-		end
+	checkForBuyPriest(game, order, orderResult, skipThisOrder, addNewOrder);
+	checkIfPriestCanConvertArmies(game. order, orderResult. skipThisOrder, addNewOrder);
+end
 
-		if (order.CostOpt == nil) then
-			return; --shouldn't ever happen, unless another mod interferes
-		end
-
-		local costFromOrder = order.CostOpt[WL.ResourceType.Gold]; --this is the cost from the order.  We can't trust this is accurate, as someone could hack their client and put whatever cost they want in there.  Therefore, we must calculate it ourselves, and only do the purchase if they match
-		local realCost = Mod.Settings.CostToBuyPriest;
-
-		if (realCost > costFromOrder) then
-			return; --don't do the purchase if their cost didn't line up.  This would only really happen if they hacked their client or another mod interfered
-		end
-
-		local numPriestsAlreadyHave = 0;
-		for _,ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			if (ts.OwnerPlayerID == order.PlayerID) then
-				numPriestsAlreadyHave = numPriestsAlreadyHave + NumPriestsIn(ts.NumArmies);
-			end
-		end
-
-		if (numPriestsAlreadyHave >= Mod.Settings.MaxPriests) then
-			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipping priest purchase since max is ' .. Mod.Settings.MaxPriests .. ' and you have ' .. numPriestsAlreadyHave));
-			return; --this player already has the maximum number of priests possible, so skip adding a new one.
-		end
-
-		local priestPower = Mod.Settings.PriestPower;
-
-		local builder = WL.CustomSpecialUnitBuilder.Create(order.PlayerID);
-		builder.Name = 'Priest';
-		builder.IncludeABeforeName = true;
-		builder.ImageFilename = 'robe.png';
-		builder.AttackPower = priestPower;
-		builder.DefensePower = priestPower;
-		builder.CombatOrder = 3415; --defends commanders
-		builder.DamageToKill = priestPower;
-		builder.DamageAbsorbedWhenAttacked = priestPower;
-		builder.CanBeGiftedWithGiftCard = true;
-		builder.CanBeTransferredToTeammate = true;
-		builder.CanBeAirliftedToSelf = true;
-		builder.CanBeAirliftedToTeammate = true;
-		builder.IsVisibleToAllPlayers = false;
-
-		local terrMod = WL.TerritoryModification.Create(targetTerritoryID);
-		terrMod.AddSpecialUnits = {builder.Build()};
-
-		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Purchased a priest', {}, {terrMod}));
+function checkForBuyPriest(game, order, orderResult, skipThisOrder, addNewOrder)
+	if not (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'BuyPriest_')) then
+		return;
 	end
-	if order.proxyType == "GameOrderAttackTransfer" then
-		if Mod.Settings.Offensive then
-			if orderResult.IsAttack and hasNoPriest(game.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies) then
-				local fromTerr = game.ServerGame.LatestTurnStanding.Territories[order.From];
-				local terrMod = WL.TerritoryModification.Create(order.From);
-				local p;
-				terrMod.AddArmies = round(orderResult.AttackingArmiesKilled.NumArmies * (Mod.Settings.Percentage / 100));
-				p = fromTerr.OwnerPlayerID;
-				if terrMod.AddArmies ~= nil and terrMod.AddArmies > 0 then
-					local event = WL.GameOrderEvent.Create(p, "priest converted " .. terrMod.AddArmies .. " armies", {}, {terrMod});
-					addNewOrder(event, true);
-				end
-			end
-		end
-		if Mod.Settings.Defensive then
-			if orderResult.IsAttack and hasNoPriest(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies) then
-				if(orderResult.IsSuccessful == false)then
-					local toTerr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-					local terrMod = WL.TerritoryModification.Create(order.To);
-					local p;
-					terrMod.AddArmies = round(orderResult.AttackingArmiesKilled.NumArmies * (Mod.Settings.Percentage / 100));
-					p = toTerr.OwnerPlayerID;
-					if terrMod.AddArmies ~= nil and terrMod.AddArmies > 0 then
-						local event = WL.GameOrderEvent.Create(p, "priest converted " .. terrMod.AddArmies .. " of the attacking armies", {}, {terrMod});
-						addNewOrder(event, true);
-					end
-				end
-			end
+
+	-- look for the order that we inserted in Client_PresentCommercePurchaseUI
+	-- in Client_PresentMenuUI, we stuck the territory ID after BuyPriest_. Break it out and parse it to a number.
+
+	print(string.sub(order.Payload, 11));
+
+	local targetTerritoryID = tonumber(string.sub(order.Payload, 11));
+	local targetTerritoryStanding = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID];
+
+	if targetTerritoryStanding.OwnerPlayerID ~= order.PlayerID then
+		-- can only buy a priest onto a territory you control
+		return;
+	end
+
+	if not order.CostOpt then
+		-- shouldn't ever happen, unless another mod interferes
+		return;
+	end
+
+	local costFromOrder = order.CostOpt[WL.ResourceType.Gold]; -- this is the cost from the order.  We can't trust this is accurate, as someone could hack their client and put whatever cost they want in there.  Therefore, we must calculate it ourselves, and only do the purchase if they match
+	local realCost = Mod.Settings.CostToBuyPriest;
+
+	if realCost > costFromOrder then
+		-- don't do the purchase if their cost didn't line up.  This would only really happen if they hacked their client or another mod interfered
+		return;
+	end
+
+	local numPriestsAlreadyHave = 0;
+
+	for _, ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+		if ts.OwnerPlayerID == order.PlayerID then
+			numPriestsAlreadyHave = numPriestsAlreadyHave + NumPriestsIn(ts.NumArmies);
 		end
 	end
+
+	if numPriestsAlreadyHave >= Mod.Settings.MaxPriests then
+		-- this player already has the maximum number of priests possible, so skip adding a new one.
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipping Priest purchase since max is ' .. Mod.Settings.MaxPriests .. ' and you have ' .. numPriestsAlreadyHave));
+		return;
+	end
+
+	local builder = WL.CustomSpecialUnitBuilder.Create(order.PlayerID);
+	local priestPower = Mod.Settings.PriestPower;
+
+	builder.Name = 'Priest';
+	builder.IncludeABeforeName = true;
+	builder.ImageFilename = 'robe.png';
+	builder.AttackPower = priestPower;
+	builder.DefensePower = priestPower;
+	builder.CombatOrder = 3415; -- defends commanders
+	builder.DamageToKill = priestPower;
+	builder.DamageAbsorbedWhenAttacked = priestPower;
+	builder.CanBeGiftedWithGiftCard = true;
+	builder.CanBeTransferredToTeammate = true;
+	builder.CanBeAirliftedToSelf = true;
+	builder.CanBeAirliftedToTeammate = true;
+	builder.IsVisibleToAllPlayers = false;
+
+	local terrMod = WL.TerritoryModification.Create(targetTerritoryID);
+
+	terrMod.AddSpecialUnits = {builder.Build()};
+
+	addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Purchased a Priest', {}, {terrMod}));
+end
+
+function checkIfPriestCanConvertArmies(game. order, orderResult. skipThisOrder, addNewOrder)
+	if not (order.proxyType == 'GameOrderAttackTransfer') then
+		return;
+	end
+
+	if Mod.Settings.Offensive then
+		makePriestConvertArmies(game, order, orderResult, skipThisOrder, addNewOrder, order.From, false);
+	end
+
+	if Mod.Settings.Defensive then
+		makePriestConvertArmies(game, order, orderResult, skipThisOrder, addNewOrder, order.To, true);
+	end
+end
+
+function makePriestConvertArmies(game, order, orderResult, skipThisOrder, addNewOrder, targetTerritoryID, isDefensive)
+	if isDefensive and not orderResult.IsSuccessful) then
+		return;
+	end
+
+	local targetTerritory = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID];
+
+	if not (orderResult.IsAttack and hasNoPriest(targetTerritory.NumArmies)) then
+		return;
+	end
+
+	local armiesToAdd = round(orderResult.AttackingArmiesKilled.NumArmies * (Mod.Settings.Percentage / 100));
+
+	if armiesToAdd <= 0 then
+		return;
+	end
+
+	local terrMod = WL.TerritoryModification.Create(targetTerritoryID);
+	local p = targetTerritory.OwnerPlayerID;
+	local msg = 'Priest converted ' .. terrMod.AddArmies .. ((isDefensive and ' of the attacking') or '') .. ' armies';
+	local event = WL.GameOrderEvent.Create(p, msg, {}, {terrMod});
+
+	addNewOrder(event, true);
+end
+
+function isPriest(specialUnit)
+	return specialUnit.proxyType == 'CustomSpecialUnit' and specialUnit.Name == 'Priest';
 end
 
 function NumPriestsIn(armies)
 	local ret = 0;
-	for _,su in pairs(armies.SpecialUnits) do
-		if (su.proxyType == 'CustomSpecialUnit' and su.Name == 'Priest') then
+
+	for _, su in pairs(armies.SpecialUnits) do
+		if isPriest(su) then
 			ret = ret + 1;
 		end
 	end
+
 	return ret;
 end
 
 function hasNoPriest(armies)
 	for _, sp in pairs(armies.SpecialUnits) do
-		if (sp.proxyType == "CustomSpecialUnit" and sp.Name == "Priest") then
+		if isPriest(sp) then
 			return true;
 		end
 	end
-	return false;
 end
 
 function round(n)
